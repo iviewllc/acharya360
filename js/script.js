42ef6e18723d01d3f3534bab517ffb9cd06f2a3d
Madhusudhan Samala <samala.madhusudhan@gmail.com>
2025-04-09 07:58:26 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -4,9 +4,8 @@
         // Event listener for form submission
         document.getElementById('contact-form').addEventListener('submit', function(event) {
             event.preventDefault();
-
             // Sending form data using EmailJS
-            emailjs.sendForm("service_dnm3t6l", "template_6po1xu9", this)
+            emailjs.sendForm("service_25fdary", "template_iv6x4n8", this)
                 .then(() => {
                     alert("Message Sent Successfully!"); // Success message
                 }, (err) => {
